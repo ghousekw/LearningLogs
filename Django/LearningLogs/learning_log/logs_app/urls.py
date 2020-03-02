37e3f72404f6c839725a62150c48193948a1e397
ghousekw <gmshaik.kw@gmail.com>
2020-03-02 13:42:27 +0300

    create a link to show all entries related to that topic

--- Django/LearningLogs/learning_log/logs_app/urls.py
+++ Django/LearningLogs/learning_log/logs_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('topics/', views.topics, name="topics"),
+    path('topics/', views.topics, name="topics"),  # show all entries
+    path('topics/<str:topic_id>', views.topic, name="topic"),  # show individual entries
 ]

4b97a14b55d0fc42d87f20ee8f4d490d28bce6be
ghousekw <gmshaik.kw@gmail.com>
2020-03-02 12:49:11 +0300

    create a html page for topic and set urls paths

--- Django/LearningLogs/learning_log/logs_app/views.py
+++ Django/LearningLogs/learning_log/logs_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 def index(request):
     """Home page for learning log"""
     return render(request, 'logs_app/index.html')
+
+
+def topics(request):
+    """Show all topics from the database"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'logs_app/topics.html', context)
+
